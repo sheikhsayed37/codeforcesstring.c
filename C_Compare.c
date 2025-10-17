4a5975b190ca736728922b0982212b427c808418
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-17 22:26:18 +0600

    223

---
+++ C_Compare.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    char st[1001],sd[1222];
+    scanf("%s %s",&st,&sd);
+     int value = strcmp(st,sd);
+    if(st>0)
+
+    
+    {
+
+     printf("%s",st);
+
+    }
+    else if(st<0)
+
+    {
+        printf("%s",st);
+    }
+}
--- E_Count.c
+++ E_Count.c
@@ -6,6 +6,7 @@ int main()
     scanf("%s",&st);
     int length=strlen(st);   //string e space count kore
     int sum=0;
+
     for(int i=0;i<length;i++)
     {
       sum =sum+st[i]-'0';
