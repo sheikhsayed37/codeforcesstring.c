31b29df95fdc012fd431a83f3ecc18825b6c3870
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-17 22:24:15 +0600

    11

--- E_Count.c
+++ E_Count.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char st[1000001];
+    scanf("%s",&st);
+    int length=strlen(st);   //string e space count kore
+    int sum=0;
+    for(int i=0;i<length;i++)
+    {
+      sum =sum+st[i]-'0';
+    }
+    printf("%d",sum);
+}
