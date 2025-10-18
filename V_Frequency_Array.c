92ced389e7477e3fbab50ec784c4eed23a6a770a
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-18 19:51:00 +0600

    new211

---
+++ V_Frequency_Array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d ",&a[i]);
+
+    }
+    int fre[100001]={0};
+    int value;
+    for(int i=0;i<n;i++)
+    {
+        value=a[i];
+        fre[value]++;
+    }
+    for(int i=1;i<=m;i++)
+    {
+        printf("%d\n",fre[i]);
+    }
+
+}
+
+
